5490590c8f1b140baccd8380fe1da83df5c64157
Bahawal Khan <bahwalkhanmangal002@gmail.com>
2025-06-27 22:40:29 +0500

    modified bubblesort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,10 +1,12 @@
-arr = [2,3,4,5,6,7,88,8,1]
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
 
-n = len(arr)
-
-for i in range(n-1):
-    for j in range(n-i-1):
-        if arr[j] > arr[j+1]:
-            arr[j],arr[j+1] = arr[j+1],arr[j]
-        
-print("sorted array: ",arr)
+arr = [9,8,7,6,5,4,3]
+print(arr)
+result = bubbleSort(arr)
+print(result)
